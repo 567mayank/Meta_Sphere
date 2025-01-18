210c8bc2679d2ef24f524a5232dcd01c4573055a
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2025-01-18 17:37:36 +0530

    add multiple socket connections

--- client/src/CoordinateManager.jsx
+++ client/src/CoordinateManager.jsx
@@ -1,31 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useSocket } from './SocketContext';
 
-const CoordinateManager = ({ onUpdate }) => {
+const CoordinateManager = ({ coordinates, onUpdate }) => {
     const socket = useSocket();
-    const [coordinates, setCoordinates] = useState({});
-    const [others, setOthers] = useState({}); // Track other players' positions
+    const [others, setOthers] = useState({});
 
-    // Send updated coordinates to the server
-    const sendCoordinates = (newCoordinates) => {
-        if (socket) {
-            socket.emit('sprite-move', newCoordinates);
-        }
-    };
-
-    // Listen for updates from other players
     useEffect(() => {
         if (socket) {
+            // Emit player coordinates 
+            if (coordinates) {
+                socket.emit('sprite-move', coordinates);
+            }
+
+            //  updates from other players
             socket.on('sprite-update', (data) => {
                 setOthers((prev) => ({ ...prev, [data.id]: data }));
-                onUpdate(data);
+                onUpdate(data); // Notify parent component
             });
-        }
 
-        return () => {
-            if (socket) socket.off('sprite-update');
-        };
-    }, [socket, onUpdate]);
+        
+            return () => {
+                socket.off('sprite-update');
+            };
+        }
+    }, [socket, coordinates, onUpdate]);
 
     return null; 
 };
--- server/src/socket.js
+++ server/src/socket.js
@@ -1,9 +1,31 @@
 export function setupSocket(io) {
+    let players = {}; 
+
     io.on('connection', (socket) => {
-      console.log('A user connected:', socket.id);
-  
-      socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id);
-      });
+        console.log('A user connected:', socket.id);
+
+     
+        socket.emit('sprite-update', Object.values(players));
+
+        
+        socket.on('sprite-move', (data) => {
+            
+            players[socket.id] = data;
+
+            
+            io.emit('sprite-update', { id: socket.id, ...data });
+
+            console.log(`Player ${socket.id} moved to x: ${data.x}, y: ${data.y}`);
+        });
+
+       
+        socket.on('disconnect', () => {
+            console.log('A user disconnected:', socket.id);
+          
+            delete players[socket.id];
+
+            
+            io.emit('sprite-update', { id: socket.id, x: null, y: null });
+        });
     });
-  }
+}
